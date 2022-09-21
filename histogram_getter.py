5c4adc891b92313a07cedd758d7b9d508b777360
Francisco Sili <francisco.sili@cern.ch>
2022-09-21 17:05:05 +0200

    different names depending which histogram type

--- histogram_getter.py
+++ histogram_getter.py
@@ -237,6 +237,7 @@ class histogram_getter:
                     if self.remove_var_cut and variable in _selection and variable != 'cuts':
                         _selection = '&&'.join([cut for cut in _selection.split('&&') if not split_cut(cut)[0] == variable])
 
+
                     # change selection and variable for systematics
                     if syst != 'Nom' and self.systematics.affects_kinematics(syst):
                         for var in self.systematics.get_affected_variables(syst):
@@ -352,12 +353,14 @@ class histogram_getter:
                     # variables dictionary
                     hist_type     = 'histogram'
                     prof_variable = is_profile_variable(variable)
+                    hist_class    = 'th1d' if not is_2d_variable(variable) else 'th2d'
                     if prof_variable:
                         # the form of `variable` is the same as for any 2d histogram    vx:vy
                         hist_type = 'profile'
+                        hist_class= 'tprofile'
                         variable  = prof_variable
                     
-                    # name to use in histograms names
+                    # name to use in histograms names and to get binning
                     variable_name = variable
                     if variable not in leaves and 'lumi' not in variable:
                         # check if variable has the form of a 2d histogram
@@ -367,7 +370,8 @@ class histogram_getter:
                             vy = get_var_function(vy) if get_var_function(vy) else vy
                             variable_name = vx + ':' + vy
                         else:
-                            variable_name = get_var_function(variable)
+                            if get_var_function(variable):
+                                variable_name = get_var_function(variable)
                     
 
                     # get binning
@@ -378,11 +382,11 @@ class histogram_getter:
 
                     # name to avoid the ROOT warning, not used
                     if self.use_skim:
-                        hname = f'h___{name}___{systname}__{region}__{get_escaped_variable(variable_name)}'
+                        hname = f'{hist_class}___{name}___{systname}__{region}__{get_escaped_variable(variable_name)}'
                     elif dsid_str:
-                        hname = f'h___{dsid_str}___{systname}__{region}__{get_escaped_variable(variable_name)}'
+                        hname = f'{hist_class}___{dsid_str}___{systname}__{region}__{get_escaped_variable(variable_name)}'
                     else:
-                        hname = f'h___{name}___{systname}__{region}__{get_escaped_variable(variable_name)}'
+                        hname = f'{hist_class}___{name}___{systname}__{region}__{get_escaped_variable(variable_name)}'
                     
                     
                     
